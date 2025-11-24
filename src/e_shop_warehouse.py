bb7c34b62c719f4d95c1aca73f3be66ce32e5a77
pauliusser <pauliusser@gmail.com>
2025-11-24 14:20:24 +0200

    e_shop_warehouse.py baigtas

--- src/e_shop_warehouse.py
+++ src/e_shop_warehouse.py
@@ -1,4 +1,5 @@
 import random
+import csv
 
 def duomenuNuskaitymas(filepath):
   with open(filepath, "r", encoding="utf-8") as f:
@@ -6,13 +7,13 @@ def duomenuNuskaitymas(filepath):
     data = [l.strip().split(";") for l in lines]
   return data
 
-data = duomenuNuskaitymas("./files/prekes250.txt")
-# print(data)
 
 
-def prekiuGeneravimas(data, count):
+
+def prekiuGeneravimas(data, filepath):
   kat = sorted(set([d[1] for d in data]))
   pre = sorted(set([d[0] for d in data]))
+  count = random.randint(0, 200)
 
   # kadangi kategorijų yra labai daug ir prekių palyginus mažai
   # t.y.:
@@ -28,24 +29,19 @@ def prekiuGeneravimas(data, count):
   for i in range(count):
     rndInts[random.randint(0, maxIndex)] += 1
 
-  c = 0
-  dataList = []
-  for index, k in enumerate(kat):
-    
-    i = rndInts[index]
-    
-    if i != 0:
-      for j in range(i):
-        if c == count: break
-        rndPre = pre[random.randint(0, len(pre) - 1)]
-        id = k[0].upper() + rndPre[0].upper() + str(index + 1) + str(j + 1).zfill(4)
-        rndKaina = round(random.uniform(0.99,999.99), 2)
-        rndLik = random.randint(0, 200)
-        dataList.append([id, rndPre, k, rndKaina, rndLik])
-        c += 1
+  with open(filepath, "w", newline="", encoding="utf-8") as f:
+    writer = csv.writer(f)
+    writer.writerow(["ID", "prekė", "kategorija", "kaina", "likutis"])
 
-  for d in dataList:
-    print(d)
+    for index, k in enumerate(kat):    
+      i = rndInts[index]    
+      if i != 0:
+        for j in range(i):
+          rndPre = pre[random.randint(0, len(pre) - 1)]
+          id = k[0].upper() + rndPre[0].upper() + str(index + 1) + str(j + 1).zfill(4)
+          rndKaina = round(random.uniform(0.99,999.99), 2)
+          rndLik = random.randint(0, 200)
+          writer.writerow([id, rndPre, k, rndKaina, rndLik])
 
   # is pradziu bandziau duomenis pertvarkyti, turbut ne taip supratau uzduoti
   # palieku uzkomentuota dali kur pertvarko jau turima sarasa (gaila trinti)
@@ -67,4 +63,79 @@ def prekiuGeneravimas(data, count):
   #     finalData.append([id, *d[:3], likutis])
   # return finalData
 
-prekiuGeneravimas(data, 20)
+def prekiuNuksaitymas(filepath):
+  prekes = []
+
+  with open(filepath, "r", encoding="utf-8") as f:
+    lines = f.readlines()
+    header = lines[0].strip().split(",")    
+    for i, line in enumerate(lines):
+      if i > 0:
+        line = lines[i].strip().split(",")
+        prekes.append({
+          header[0]: line[0],           # ID
+          header[1]: line[1],           # prekė
+          header[2]: line[2],           # kategorija
+          header[3]: float(line[3]),    # kaina
+          header[4]: int(line[4])       # likutis
+        })
+
+  return prekes
+
+def analizuotiA (prkes):
+  brangiausiaPr = max(prkes, key=lambda p: p["kaina"])
+  deficitinePr = min(prekes, key=lambda p: p["likutis"])
+  kainuSum = sum([p["kaina"] * p["likutis"] for p in prekes])
+  print(f"""
+brangiausia prekė: {brangiausiaPr["prekė"]} - {brangiausiaPr["kaina"]}eur.
+prekė su mažiausiu likučiu {deficitinePr["prekė"]} - {deficitinePr["likutis"]}vnt.
+sandėlyje liko prekių už: {kainuSum}eur.""")
+
+def analizuotiB (prekes):
+  kategorijos = sorted(set([p["kategorija"] for p in prekes]))
+  print("\nkategorijos:")
+  for i, kat in enumerate(kategorijos):
+    print(f"{i + 1}. {kat}")
+  pas = int(input("pasirinkite kategorijos numerį: ")) - 1
+  k = kategorijos[pas]
+  preSum = sum([1 for p in prekes if p["kategorija"] == k ])
+  likSum = sum([p["likutis"] for p in prekes if p["kategorija"] == k])
+  vidKain = round(sum([p["kaina"] for p in prekes if p["kategorija"] == k ]) / preSum, 2)
+  print(f"""
+kategorijoje - {k}:
+skirtingų prekių kiekis: {preSum} prekės
+visų prekių bendras likutis: {likSum} vnt.
+vidutinė prekių kaina: {vidKain} eur.""")
+
+def kurtiAtaskaita(prekes, filepath):
+  top10brang = sorted(prekes, key=lambda p: p["kaina"], reverse=True)[:10]
+  likSar = [p for p in prekes if p["likutis"] < 5]
+  sandVert = sum([p["kaina"] for p in prekes])
+
+  def drawHorLine():
+    f.write("+" + "-" * 10 + "+" + "-" * 17 + "+" + "-" * 17 + "+" + "-" * 9 + "+" + "-" * 9 + "+\n")
+  def drawTable(list):
+      drawHorLine()
+      f.write(f"| {"ID":<8} | {"prekė":>15} | {"kategorija":>15} | {"kaina":>7} | {"likutis":>7} |\n")
+      for p in list:
+        drawHorLine()
+        f.write(f"| {p["ID"]:<8} | {p["prekė"]:>15} | {p["kategorija"]:>15} | {p["kaina"]:>7} | {p["likutis"]:>7} |\n")
+      drawHorLine()
+
+  with open(filepath, "w", encoding="utf-8") as f:
+    f.write("top 10 brangiausių prekių:\n")
+    drawTable(top10brang)
+    f.write("\nprekių su mažiausiu likučiu sąrašas:\n")
+    if len(likSar) == 0: 
+      f.write("\nprekių su mažesniu nei 5 vnt. likučiu nėra\n")
+    else: 
+      sortedLikSar = sorted(likSar, key=lambda p: p["likutis"])
+      drawTable(sortedLikSar)
+    f.write(f"\nVisų sandėlio prekių vertė: {sandVert} eur.")
+
+data = duomenuNuskaitymas("./files/prekes250.txt")
+prekiuGeneravimas(data, "./files/sandėlis.csv")
+prekes = prekiuNuksaitymas("./files/sandėlis.csv")
+# analizuotiA(prekes)
+# analizuotiB(prekes)
+kurtiAtaskaita(prekes,"./files/sandelio_ataskaita.txt")
